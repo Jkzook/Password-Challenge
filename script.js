6508b0c4bc03649b1192b5bba0077877a68e75d2
Jkzook <119649225+Jkzook@users.noreply.github.com>
2023-01-24 22:39:41 -0700

    innerhtml

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 // Assignment code here
 console.log("your external JavaScript file")
 
-
+var password=document.getElementById("password");
 // Get references to the #generate element
+function
 var generateBtn = document.querySelector("#generate");
 var chars = "012345678abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYS"
 var passwordLength = 8;
 var password = "";
+for (var i =0; i<= passwordLength; i++) {var randomNumber = Math.floor(Math.random() * chars.length);
+password =+ chars.substring(randomNumber, randomnumber +1);}
+document.getElementById("password".value) = password;
 
 // Write password to the #password input
-function writePassword() {
+function genPassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+
 
   passwordText.value = password;
 
